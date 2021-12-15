ea2df7a8123865aadffb5df01e8ce7edb9f8170d
Rexi <68111233+RexiRexii@users.noreply.github.com>
2021-12-15 12:16:41 +0300

    Create functions.hpp

---
+++ functions.hpp
@@ -0,0 +1,38 @@
+#include "headers.hpp"
+#include <Windows.h>
+
+void r_lua_pushnil(std::uintptr_t rL)
+{
+	r_setnilvalue(*reinterpret_cast<r_TValue**>(rL + offsets::top));
+	r_incr_top(rL);
+}
+
+void r_lua_pushthread(std::uintptr_t rL)
+{
+	r_setthvalue(*reinterpret_cast<r_TValue**>(rL + offsets::top), rL);
+	r_incr_top(rL);
+}
+
+void r_lua_pushboolean(std::uintptr_t rL, bool b)
+{
+	r_setbvalue(*reinterpret_cast<r_TValue**>(rL + offsets::top), b);
+	r_incr_top(rL);
+}
+
+void r_lua_pushnumber(std::uintptr_t rL, std::double_t num)
+{
+	r_setnvalue(*reinterpret_cast<r_TValue**>(rL + offsets::top), num);
+	r_incr_top(rL);
+}
+
+void r_lua_pushinteger(std::uintptr_t rL, std::uint32_t n)
+{
+	r_setnvalue(*reinterpret_cast<r_TValue**>(rL + offsets::top), cast_num(n));
+	r_incr_top(rL);
+}
+
+void r_lua_pushlightuserdata(std::uintptr_t rL, void* p) 
+{
+	r_setpvalue(*reinterpret_cast<r_TValue**>(rL + offsets::top), p);
+	r_incr_top(rL);
+}
